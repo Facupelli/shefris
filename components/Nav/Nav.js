b9080ddca2b14ae7e5ca3e9ef35fac6d92cf0d3b
Facupelli <facundopellicer4@gmail.com>
2022-07-12 19:52:45 -0300

    cart icon svg

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,13 +1,13 @@
-import s from './Nav.module.scss'
+import CartIcon from "./CartIcon";
+import s from "./Nav.module.scss";
 
-export default function Nav () {
-
-    return(
-        <nav>
-            <p>SHEFRIS</p>
-            <div>
-                <p>CARRITO</p>
-            </div>
-        </nav>
-    )
+export default function Nav() {
+  return (
+    <nav className={s.container}>
+      <p>SHEFRIS</p>
+      <div>
+        <CartIcon />
+      </div>
+    </nav>
+  );
 }
